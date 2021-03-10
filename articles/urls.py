7ba2ec9899e33a47379c9e13c7a2b39e8fd9e615
dominatordq <dominatordq@gmail.com>
2021-03-10 14:20:07 -0700

    Added URL parameters so the program identifies any slug after '.../articles/<slug>'

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.article_list)
+    path('', views.article_list),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail),   # [\w-]+ -- \w = any word or number, - = hyphens can be used, + = any length
+    #path('<slug:slug>', views.article_detail)
 ]
