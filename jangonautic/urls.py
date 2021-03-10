b71a0bc94bc1afed9a4bb98c0cc4c05b1e9893b8
dominatordq <dominatordq@gmail.com>
2021-03-10 12:26:48 -0700

    Added static files to the project involving a stylesheet and an image for the background

--- jangonautic/urls.py
+++ jangonautic/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # allows Django to handle the serving of static files
 from django.urls import path, include
 from . import views
 
@@ -8,3 +9,5 @@ urlpatterns = [
     path('about/', views.about),
     path('', views.home)    # home page
 ]
+
+urlpatterns += staticfiles_urlpatterns()
