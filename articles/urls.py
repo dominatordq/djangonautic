edb560dbb74b3a71d79ce60287f5c53c2126d503
dominatordq <dominatordq@gmail.com>
2021-03-10 15:21:33 -0700

    Added Named URLs to articles, so each one is matched with a slug

--- articles/urls.py
+++ articles/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.article_list),
-    url(r'^(?P<slug>[\w-]+)/$', views.article_detail),   # [\w-]+ -- \w = any word or number, - = hyphens can be used, + = any length
-    #path('<slug:slug>', views.article_detail)
+    path('', views.article_list, name="list"),
+    #url(r'^(?P<slug>[\w-]+)/$', views.article_detail),   # [\w-]+ -- \w = any word or number, - = hyphens can be used, + = any length
+    path('<slug:slug>/', views.article_detail, name="detail")
 ]
